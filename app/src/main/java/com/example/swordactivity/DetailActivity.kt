30c4ab63b146e2dcf50a1af35bb9d854e40ea680
reson.li <reson.li@corp.to8to.com>
2019-09-04 18:00:27 +0800

    第二题

--- app/src/main/java/com/example/swordactivity/DetailActivity.kt
+++ app/src/main/java/com/example/swordactivity/DetailActivity.kt
@@ -11,8 +11,8 @@ class DetailActivity : AppCompatActivity() {
     private val arr = Array(10) { IntArray(10) }
 
     /**
-     在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
-     每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+     * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+     * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
     **/
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/swordactivity/TwoActivity.kt
@@ -0,0 +1,16 @@
+package com.example.swordactivity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class TwoActivity : AppCompatActivity() {
+
+    /**
+     * 请实现一个函数，将一个字符串中的每个空格替换成“%20”。
+     * 例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
+     * **/
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
